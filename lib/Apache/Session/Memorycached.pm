08b632ef2b5dcc02198ff8a6fe10bd0d7da7805a
eric GERMAN <germanlinux@yahoo.fr>
2004-10-06 09:27:56 +0000

    Import of EGERMAN/Apache-Session-Memorycached-1.0 from CPAN.
    gitpan-cpan-distribution: Apache-Session-Memorycached
    gitpan-cpan-version:      1.0
    gitpan-cpan-path:         EGERMAN/Apache-Session-Memorycached-1.0.tar.gz
    gitpan-cpan-author:       EGERMAN
    gitpan-cpan-maturity:     released

---
+++ lib/Apache/Session/Memorycached.pm
@@ -0,0 +1,82 @@
+#############################################################################
+#
+# Apache::Session::Memorycached
+# Apache persistent user sessions on the network with memcached
+# Copyright(c) eric german <germanlinux@yahoo.fr>
+# Distribute under the Artistic License
+#
+############################################################################
+
+package Apache::Session::Memorycached;
+
+use strict;
+use vars qw(@ISA $VERSION);
+
+$VERSION = '1.0';
+@ISA = qw(Apache::Session);
+
+use Apache::Session;
+use Apache::Session::Generate::MD5;
+use Apache::Session::Lock::Memorycached;
+use Apache::Session::Store::Memorycached;
+
+sub populate {
+    my $self = shift;
+
+    $self->{object_store} = new Apache::Session::Store::Memorycached $self;
+    $self->{lock_manager} = new Apache::Session::Lock::Memorycached $self;
+    $self->{generate}     = \&Apache::Session::Generate::MD5::generate;
+    $self->{validate}     = \&Apache::Session::Generate::MD5::validate;
+    $self->{serialize}    = \&Apache::Session::Memorycached::none;
+    $self->{unserialize}  = \&Apache::Session::Memorycached::none;
+
+    return $self;
+}
+
+sub none {
+    my $self    = shift;
+    my $session = shift;
+return;
+ }
+ sub DESTROY {
+    my $self = shift;
+    
+    $self->save;
+    $self->{object_store}->close;
+    $self->release_all_locks;
+}
+
+1;
+
+
+=pod
+
+=head1 NAME
+
+Apache::Session::Memorycached - An implementation of Apache::Session
+
+=head1 SYNOPSIS
+
+ use Apache::Session::Memorycached;
+ 
+    tie %session, 'Apache::Session::Memorycached', $cookie, {
+          'servers' => ["10.75.1.19:11211"], #all write operations
+          'local'  =>  ["localhost:11211"],  #read-only operations
+
+     };
+
+=head1 DESCRIPTION
+
+This module is an implementation of Apache::Session.  It uses the memcached system backing
+store .  You may specify servers (principal)  and locals caches for locking in arguments to the constructor. See
+the example, and the documentation for Apache::Session::Store::Memorycached
+
+
+=head1 AUTHOR
+
+This module was written by eric german <germanlinux@yahoo.fr>.
+
+=head1 SEE ALSO
+
+L<Apache::Session::DB_File>, L<Apache::Session::Flex>,
+L<Apache::Session::MySQL>, L<Apache::Session::Postgres>, L<Apache::Session>
---
+++ lib/Apache/Session/Store/Memorycached.pm
@@ -0,0 +1,192 @@
+#############################################################################
+#
+# Apache::Session::Store::Memorycached
+# Implements session object storage via memcached 
+# Copyright(c) eric german <germanlinux@yahoo.fr>     
+# Distribute under the Artistic License
+#
+############################################################################
+
+package Apache::Session::Store::Memorycached;
+
+use strict;
+use Symbol;
+use Cache::Memcached;
+use vars qw($VERSION);
+
+$VERSION = '1.0';
+
+
+sub new {
+    my $class = shift;
+    my $self;
+    $self->{opened} = 0;
+    
+    return bless $self, $class;
+}
+
+
+sub insert {
+    my $self    = shift;
+    my $session = shift;
+ 
+ my $ryserver = $session->{args}->{servers};
+ my $ryserverlocal = $session->{args}->{local};
+ my $memd= new Cache::Memcached  { 'servers' => $ryserver };
+ my $ident = $session->{data}->{_session_id}; 
+ my $rhash = $session->{data};
+ $memd->set($ident,$rhash);
+ $memd->disconnect_all();
+  if ($ryserverlocal)
+     {
+ my $memdlocal= new Cache::Memcached  { 'servers' => $ryserverlocal };
+ my $identlocal = $session->{data}->{_session_id}; 
+ my $rhashlocal = $session->{data};
+ $memdlocal->set($identlocal,$rhashlocal);
+ $memdlocal->disconnect_all();
+     }
+$self->{opened} = 1;
+ 
+ }
+
+sub update {
+    my $self    = shift;
+    my $session = shift;
+ 
+ my $ryserver = $session->{args}->{servers};
+ my $ryserverlocal = $session->{args}->{local};
+ my $memd= new Cache::Memcached  { 'servers' => $ryserver };
+ my $ident = $session->{data}->{_session_id} ;
+ my $rhash = $session->{data};
+ $memd->set($ident,$rhash);
+ $memd->disconnect_all();
+  if ($ryserverlocal)
+     {
+ my $memdlocal= new Cache::Memcached  { 'servers' => $ryserverlocal };
+ my $identlocal = $session->{data}->{_session_id}; 
+ my $rhashlocal = $session->{data};
+ $memdlocal->set($identlocal,$rhashlocal);
+ $memdlocal->disconnect_all();
+     }
+ $self->{opened} = 1;
+}
+
+sub materialize {
+    my $self    = shift;
+    my $session = shift;
+my $ryserver = $session->{args}->{servers};
+my $rhash; 
+my $ryserverlocal = $session->{args}->{local};
+  if ($ryserverlocal)
+     {
+ my $memdlocal= new Cache::Memcached  { 'servers' => $ryserverlocal };
+ my $identlocal = $session->{data}->{_session_id}; 
+ $rhash = $memdlocal->get($identlocal);
+ $memdlocal->disconnect_all();
+    }
+ unless ($rhash)
+          {
+           my $memd= new Cache::Memcached  { 'servers' => $ryserver };
+           my $ident = $session->{data}->{_session_id}; 
+           $rhash = $memd->get($ident);
+           $memd->disconnect_all();
+ ## the data is in the  principal cache notin the local cache 
+ ##  we must put data in it.
+            if ($ryserverlocal)
+                  {
+                     my $memdlocal= new Cache::Memcached  { 'servers' => $ryserverlocal };
+                     my $identlocal = $session->{data}->{_session_id}; 
+                     my $rhashlocal = $session->{data};
+                     $memdlocal->set($identlocal,$rhashlocal);
+                     $memdlocal->disconnect_all();
+                   }
+
+         }
+ $self->{opened} = 1;
+ $session->{data} =$rhash;
+    
+}    
+
+sub remove {
+    my $self    = shift;
+    my $session = shift;
+    my $ryserver = $session->{args}->{servers};
+    my $memd= new Cache::Memcached  { 'servers' => $ryserver };
+    my $ryserverlocal = $session->{args}->{local};
+    my $ident = $session->{data}->{_session_id} ;
+        $memd->delete($ident);  
+        $memd->disconnect_all();
+     if ($ryserverlocal)
+          {
+            my $memdlocal= new Cache::Memcached  { 'servers' => $ryserverlocal };
+            my $identlocal = $session->{data}->{_session_id}; 
+            $memdlocal->delete($identlocal);
+            $memdlocal->disconnect_all();
+          }
+ 
+   $self->{opened} = 0;
+    
+    }
+
+sub close {
+    my $self = shift;
+    
+    if ($self->{opened}) {
+        $self->{opened} = 0;
+    }
+}
+
+sub DESTROY {
+    my $self = shift;
+    
+    if ($self->{opened}) {    
+    }
+}
+
+1;
+
+=pod
+
+=head1 NAME
+
+Apache::Session::Store::Memorycached - Store persistent data on the network with  memcached
+
+=head1 SYNOPSIS
+
+
+ use Apache::Session::Store::Memorycached;
+ 
+ my $store = new Apache::Session::Store::Memorycached;
+ 
+ $store->insert($ref);
+ $store->update($ref);
+ $store->materialize($ref);
+ $store->remove($ref);
+
+=head1 DESCRIPTION
+
+This module fulfills the storage interface of Apache::Session.  The serialized
+objects are stored in files on your network in unused memory 
+
+=head1 OPTIONS
+
+This module requires one argument in the usual Apache::Session style.  The
+name of the option is servers, and the value is the  same of memcached .
+ Example
+
+ tie %s, 'Apache::Session::Memorycached', undef,
+    {servers  => ['mymemcachedserver:port'] };
+
+In order to optimize the network ,you can use a local memcached server.
+All read-only opération are sending fisrt at local server .If you need write ou rewrite data , the data is sending at the principal memcached sever and local cache too  for synchronisation.
+
+=head1 NOTES
+
+
+=head1 AUTHOR
+
+This module was written by eric german <germanlinux@yahoo.fr> 
+
+=head1 SEE ALSO
+
+L<Apache::Session>
